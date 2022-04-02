f7febe55043d79139fbd03a5d78a2ae7e21b714b
John-Pena <jmp1495@gmail.com>
2022-04-02 17:07:13 -0500

    initial file setups

---
+++ index.js
@@ -0,0 +1,13 @@
+// TODO: Include packages needed for this application
+
+// TODO: Create an array of questions for user input
+const questions = [];
+
+// TODO: Create a function to write a README file
+function writeFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// function call to initialize app
+init();
