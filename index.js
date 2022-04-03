c0fc38784ca4c5cc0c615165022a2923dff6696d
John-Pena <jmp1495@gmail.com>
2022-04-02 22:14:39 -0500

    allowed question to display for user, working on file saving user answer.

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const promptUser = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Write a description of your project (Required)',
+            validate: descriptionInput => {
+                if (descriptionInput) {
+                    return true;
+                } else {
+                    console.log('Please enter a description!');
+                    return false;
+                }
+            }
+        }
+    ])
+}
 
 // TODO: Create a function to write a README file
 function writeFile(fileName, data) {}
@@ -10,4 +27,5 @@ function writeFile(fileName, data) {}
 function init() {}
 
 // function call to initialize app
+promptUser();
 init();
