25cd96e210526bc9d22e6a20b0f2389fcfebceeb
John-Pena <jmp1495@gmail.com>
2022-04-03 21:18:03 -0500

    provided more questions to user to gather more information for README file

--- index.js
+++ index.js
@@ -1,18 +1,61 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
+const markdown = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
 const promptUser = () => {
     return inquirer.prompt([
         {
             type: 'input',
+            name: 'title',
+            message: 'What is the name of the project? (Required)',
+            validate: titleInput => {
+                if (titleInput) {
+                    return true;
+                } else {
+                    console.log('Please enter a title of the project!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
             name: 'description',
-            message: 'Write a description of your project (Required)',
+            message: 'Please povide a description of the project (Required)',
             validate: descriptionInput => {
                 if (descriptionInput) {
                     return true;
                 } else {
-                    console.log('Please enter a description!');
+                    console.log('Please provde a description!');
+                    return false;
+                }
+            }
+        },
+        {
+            // table of content section
+        },
+        {
+            type: 'input',
+            name: 'installation',
+            message: 'Please provide installation intructions',
+            validate: installationInput => {
+                if (installationInput) {
+                    return true;
+                } else {
+                    console.log('Please provide instructions on how to install');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'contributors',
+            message: 'Who contributed to the project?',
+            validate: contributorInput => {
+                if (contributorInput) {
+                    return true;
+                } else {
+                    console.log('Please provide a contributor');
                     return false;
                 }
             }
