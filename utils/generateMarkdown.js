82d0431e72b46a512bf846f8d587933e3494034a
John-Pena <jmp1495@gmail.com>
2022-04-04 22:49:27 -0500

    added license, badge, and used template literal to insert license section

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -3,25 +3,43 @@
 function renderLicenseBadge(license) {
     if (!license) {
         return '';
-    } else {
-        return `
-        ![badge](https://img.shields.io/badge/license-${data.license}-brightgreen)
-        `;
     }
+    return `
+        ![badge](https://img.shields.io/badge/license-${license}-blue)
+    `;
 }
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+    if (!license) {
+        return '';
+    }
+    return `
+        This application is licensed by [${license}](https://opensource.org/license/${license}).
+    `
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+    if (!license) {
+        return '';
+    }
+    return `
+        ## License
+    `;
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-    return `# ${data.title}
+    return `
+    # ${data.title}
+
+    ${renderLicenseLink}
     ${renderLicenseBadge}
+    ${renderLicenseLink}
+
 
     <a name="description"></a>
     ## Description
