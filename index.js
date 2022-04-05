25bfe3f14a0c6a981eed796d3b553a31c5000ee0
John-Pena <jmp1495@gmail.com>
2022-04-04 22:27:34 -0500

    created template literal for user input to append

--- index.js
+++ index.js
@@ -1,74 +1,78 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
+const fs = require('fs');
 const markdown = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
-const promptUser = () => {
-    return inquirer.prompt([
-        {
-            type: 'input',
-            name: 'title',
-            message: 'What is the name of the project? (Required)',
-            validate: titleInput => {
-                if (titleInput) {
-                    return true;
-                } else {
-                    console.log('Please enter a title of the project!');
-                    return false;
-                }
-            }
-        },
-        {
-            type: 'input',
-            name: 'description',
-            message: 'Please povide a description of the project (Required)',
-            validate: descriptionInput => {
-                if (descriptionInput) {
-                    return true;
-                } else {
-                    console.log('Please provde a description!');
-                    return false;
-                }
-            }
-        },
-        {
-            // table of content section
-        },
-        {
-            type: 'input',
-            name: 'installation',
-            message: 'Please provide installation intructions',
-            validate: installationInput => {
-                if (installationInput) {
-                    return true;
-                } else {
-                    console.log('Please provide instructions on how to install');
-                    return false;
-                }
-            }
-        },
-        {
-            type: 'input',
-            name: 'contributors',
-            message: 'Who contributed to the project?',
-            validate: contributorInput => {
-                if (contributorInput) {
-                    return true;
-                } else {
-                    console.log('Please provide a contributor');
-                    return false;
-                }
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the name of the project?',
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Please povide a description of the project',
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'How is the application installed?',
+    },
+    {
+        type: 'input',
+        name: 'contributors',
+        message: 'Who contributed to the project?',
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'How is this application used?',
+    },
+    {
+        type: 'input',
+        name: 'projectTestApp',
+        message: 'How do we test the application?',
+    },
+    {
+        type: 'input',
+        name: 'githubUsername',
+        message: 'What is your GitHub username?'
+    },
+    {
+        type: 'input',
+        name: 'userEmail',
+        message: 'What is your email address?'
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'Would you like to add a license? (Optional)',
+        choices: ['MIT', 'ISC', 'Apache-2.0'],
+        validate: licenseInput => {
+            if (licenseInput) {
+                return true;
+            } else {
+                console.log('Please select a license');
+                return false;
             }
         }
-    ])
-}
+    }
+];
 
-// TODO: Create a function to write a README file
-function writeFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    return inquirer.prompt(questions)
+        .then((resp) => {
+            const readME = markdown(resp);
+            fs.writeFile('./README.md', readME, err => {
+                if (err) throw new Error(err);
+                console.log('README created! Checkout README.md in the directory to view.')
+            });
+        });
+};
 
 // function call to initialize app
-promptUser();
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,14 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is not license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+    if (!license) {
+        return '';
+    } else {
+        return `
+        ![badge]()
+        `;
+    }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
@@ -13,6 +21,39 @@ function renderLicenseSection(license) {}
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
     return `# ${data.title}
+    ${renderLicenseBadge}
+
+    <a name="description"></a>
+    ## Description
+    ${data.description}
+
+    ## Table of Contents
+    1. [ Description ](#description)
+    2. [ Contributors ](#contributors)
+    3. [ Installation ](#install)
+    4. [ Usage ](#usage)
+    5. [ Test Application ](#test-app)
+    6. [ GitHub Username ](#github)
+    7. [ Email ](#email)
+
+    <a name="install"></a>
+    ## Installation
+    ${data.installation}
+
+    <a name="usage"></a>
+    ## Usage
+    ${data.usage}
+
+    <a name="test-app"></a>
+    ## Application Test
+    ${data.projectTestApp}
+
+    <a name="github"></a>
+    ## GitHub
+    ${data.githubUsername}
+
+    <a name="email"></a>
+    ${data.userEmail}
     
     `;
 }
